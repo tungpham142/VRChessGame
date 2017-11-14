323267b529d0f06b0f4eda8ad148f457c6d26356
unknown <tpv.142@gmail.com>
2017-11-14 14:11:26 -0600

    new update version

---
+++ Assets/Scripts/ColorTest.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ColorTest : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void RedChange() {
+        gameObject.GetComponent<MeshRenderer>().material.color = Color.red;
+    }
+
+    public void GreenChange()
+    {
+        gameObject.GetComponent<MeshRenderer>().material.color = Color.green;
+    }
+
+    public void BlueChange()
+    {
+        gameObject.GetComponent<MeshRenderer>().material.color = Color.blue;
+    }
+
+    public void LoadSence()
+    {
+        SceneManager.LoadScene("ChessGame");
+    }
+}
